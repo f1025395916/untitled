d0ca0c0093053900b10fb7928b44ff263eb19076
fangshuai <1025395916@qq.com>
2019-07-25 09:05:18 +0800

    #

--- 爬取案例/test1.py
+++ 爬取案例/test1.py
@@ -1,8 +1,35 @@
 import requests
-
+import time
+'''
 r = requests.get("https://a.jd.com//ajax/queryServerData.html")
 
 print(r.elapsed.microseconds)
 
 print(r.elapsed.total_seconds())
 
+print(r.elapsed.total_seconds())
+'''
+
+def print_func_time(function):
+    '''
+    计算程序运行时间
+    :param function:
+    :return:
+    '''
+
+
+    def func_time(*args, **kwargs):
+        t0 = time.clock()
+        result = function(*args, **kwargs)
+        t1 = time.clock()
+
+        print("Total running time: %s s" % (str(t1 - t0)))
+        return result
+
+    return func_time
+
+@print_func_time
+def test():
+    print(123)
+
+test()
