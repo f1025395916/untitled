d06694d1420a8a6f7bed4817c909909c1954e8a5
xiaoshuai <1025395916@qq.com>
2019-08-14 10:27:48 +0800

    2019.8.14

--- 爬取案例/test3.py
+++ 爬取案例/test3.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
-with open(r'F:\123.txt',mode="r+") as file:
-    file.write('zzzz')
-    content = file.read()
-    print("123")
-    print(content)
+
+
+
+dic_test = {'name':'小帅','age':18}
+
+print(dic_test["name"])
+for k,v in dic_test.items():
+
+    print(k+":"+str(v))
+for k in dic_test.keys():
+    print(k)
+
+
+dic_txl = {"zhangsan":"13806659465","xiaoshuai":"18660653953","xiaoming":"13306464505"}
+
+while(True):
+    str_name = input("输入要查找人姓名(可模糊查询)：")
+    for k,v in dic_txl.items():
+        if k.startswith(str_name):
+            print(k+':'+v)
+    i = input("是否继续查找(y/n?):")
+    if i =='n':
+        break
+
